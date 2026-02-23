726d3ad58d02c73f918b0aed39cbf1b489319794
Jimmy <jimmy@r3e.network>
2026-02-23 14:08:54 +0800

    fix(tests): resolve failing go tests for new miniapps

--- services/simulation/marble/simulator_other.go
+++ services/simulation/marble/simulator_other.go
@@ -507,16 +507,13 @@ func (s *MiniAppSimulator) SimulateGuardianPolicy(ctx context.Context) error {
 }
 
 func (s *MiniAppSimulator) SimulateAirdrop(ctx context.Context) error {
-	s.Logger().WithContext(ctx).Debug("Simulating builtin-airdrop")
 	return nil
 }
 
 func (s *MiniAppSimulator) SimulateDaoVoting(ctx context.Context) error {
-	s.Logger().WithContext(ctx).Debug("Simulating builtin-dao-voting")
 	return nil
 }
 
 func (s *MiniAppSimulator) SimulateGacha(ctx context.Context) error {
-	s.Logger().WithContext(ctx).Debug("Simulating builtin-gacha")
 	return nil
 }
