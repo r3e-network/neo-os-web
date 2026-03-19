96a2713ff37823ad3d467759a65f6e6b7b7b6513
Jimmy <jimmy@r3e.network>
2026-03-19 18:09:22 +0800

    Extract host app developer page cards

--- platform/host-app/components/developer/index.ts
+++ platform/host-app/components/developer/index.ts
@@ -1,3 +1,5 @@
+export { DeveloperCalloutCard } from "./DeveloperCalloutCard";
 export { DefinitionActionBar } from "./DefinitionActionBar";
 export { DefinitionModeToggle } from "./DefinitionModeToggle";
 export { TemplateMarketFilters } from "./TemplateMarketFilters";
+export { TemplateMarketCard } from "./TemplateMarketCard";
