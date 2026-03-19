99e64e25e8752f0d22a2958f14ea849e7e0128a6
Jimmy <jimmy@r3e.network>
2026-03-19 18:49:55 +0800

    Unify miniapp entry mounts

--- apps/flamingo-analytics/src/main.ts
+++ apps/flamingo-analytics/src/main.ts
@@ -1,5 +1,4 @@
-import { createApp as createVueApp } from "vue";
 import App from "./App.vue";
-const app = createVueApp(App);
-const container = document.getElementById("app");
-if (container) app.mount(container);
+import { createMiniAppEntry } from "@shared/utils/createMiniAppEntry";
+
+createMiniAppEntry(App);
